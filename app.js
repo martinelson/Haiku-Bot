1a9b9b7ca86d6727af6a398422253ab2cc1d88a2
Martin <martin.c.nelson8@gmail.com>
2020-08-24 21:07:15 -0500

    add port

--- app.js
+++ app.js
@@ -104,6 +104,11 @@ app.post("/", function(req, res){
   });
 
 });
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started successfully");
 });
